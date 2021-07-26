c2ea86b52d5ce32f4c0f9982e6c3c51cb4aa1876
JoniKanoni <jonasbottjer@mailbox.org>
2021-07-26 13:39:06 +0200

    Different methods
    added different interpolation methods,
    linear, csplice and polynomial (lagrange)

--- interpolate.py
+++ interpolate.py
@@ -1,3 +1,18 @@
-from scipy.interpolate import CubicSpline, interp1d
+'''
+Module with multiple options to interpolate a given function
+'''
 
+from scipy.interpolate import CubicSpline, interp1d, lagrange
 
+def interpolatierer(xx, yy, inttype):
+    '''
+    dgiuohdshodgiho
+    '''
+
+    if inttype == 'linear':
+        resultpot = interp1d(xx,yy, kind='linear')
+    elif inttype == 'cspline':
+        resultpot = CubicSpline(xx,yy, bc_type='natural')
+    elif inttype == 'polynomial':
+        resultpot = lagrange(xx,yy)
+    return resultpot
