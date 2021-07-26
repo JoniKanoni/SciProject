3c20987e2a4b4b1903f4486da47c527a5bd2bfde
JoniKanoni <jonasbottjer@mailbox.org>
2021-07-26 11:33:06 +0200

    1st test test
    test_reader test

--- reader.py
+++ reader.py
@@ -1,19 +1,17 @@
 '''
 Module reads data
 '''
+import time
+start_time = time.time()
 
 import os.path
 import numpy as np
-#import scipy
-
-
 
 def getdata(path):
     '''
     Reads data from input file
     '''
     inputpath = os.path.join("c:/", "users/", "boett/", "onedrive/", "dokumente/", "github/",   path, "schrodinger.inp") #pylint: disable=line-too-long
-    #data = open(inputpath, "r")
     with open(inputpath, "r") as data:
         masse = data.readline()
         masse = float(masse.replace(' # mass',''))
@@ -28,13 +26,18 @@ def getdata(path):
         firstlast = data.readline()
         firstlast = firstlast.replace('# first and last eigenvalue to print','')
         first, last = firstlast.split()
-
+        first= float(first)
+        last = float(last)
         interpolation = data.readline()
         interpolation = interpolation.replace('# interpolation type','')
-        numberinterpol = data.readline()
-        numberinterpol = numberinterpol.replace('# nr. of interpolation points and xy declarations','') #pylint: disable=line-too-long
+        interpolation = interpolation.strip()
+        numinterpol = data.readline()
+        numinterpol = numinterpol.replace('# nr. of interpolation points and xy declarations','')
+        numinterpol = numinterpol.strip()
+        numinterpol = float(numinterpol)
         rest = np.loadtxt(data.readlines())
 
-    return xmin, xmax, xnum, masse, rest, interpolation, numberinterpol, first, last
+    return masse, xmin, xmax, xnum, first, last, interpolation, numinterpol, rest
 
 print(getdata("sciproject"))
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ test_reader.py
@@ -0,0 +1,30 @@
+import reader
+import numpy as np
+
+def test_unendlich():
+    ''' Test 1 - Unendlich tiefer'''
+    massetest = 2.0
+    xmintest = -2.0
+    xmaxtest = 2.0
+    npointtest = 1999.0
+    firsttest = 1
+    lasttest = 5
+    interpolationtest = 'linear'
+    numinterpolationtest = 2
+    resttest = np.array([[-2.0, 0.0], [2.0, 0.0]]) 
+
+    masse, xmin, xmax, xnum, first, last, interpolation, numinterpol, rest = reader.getdata('sciproject')   
+
+    assert masse == massetest
+   
+    assert xmin == xmintest
+    assert xmax == xmaxtest
+    assert xnum == npointtest
+    assert first == firsttest
+    assert last == lasttest
+    assert interpolation == interpolationtest
+    assert numinterpol == numinterpolationtest
+    assert np.array_equal(rest,resttest)
+
+    return
+
