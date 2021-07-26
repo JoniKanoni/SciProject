d80b574a65ab16ca0a2529d4ad6503f48bf26924
JoniKanoni <jonasbottjer@mailbox.org>
2021-07-26 10:08:51 +0200

    Update reader.py
    code optimization

--- reader.py
+++ reader.py
@@ -1,8 +1,10 @@
-import numpy as np
-import scipy 
+'''
+Module reads data
+'''
+
 import os.path
-
-
+import numpy as np
+#import scipy
 
 
 
@@ -10,31 +12,29 @@ def getdata(path):
     '''
     Reads data from input file
     '''
-    
-    inputpath = os.path.join("c:/", "users/", "boett/", "onedrive/", "dokumente/", "github/",   path, "schrodinger.inp")
-    ei = open(inputpath, "r")
+    inputpath = os.path.join("c:/", "users/", "boett/", "onedrive/", "dokumente/", "github/",   path, "schrodinger.inp") #pylint: disable=line-too-long
+    #data = open(inputpath, "r")
+    with open(inputpath, "r") as data:
+        masse = data.readline()
+        masse = float(masse.replace(' # mass',''))
 
-    #for line in ei:
-    #    print(line) 
-    masse = ei.readline()
-    masse = float(masse.replace(' # mass',''))
+        minmax = data.readline()
+        minmax = minmax.replace('# xMin xMax nPoint','')
+        minmax = minmax.split()
+        xmin, xmax, xnum = minmax
+        xmin = float(xmin)
+        xmax = float(xmax)
+        xnum = float(xnum)
+        firstlast = data.readline()
+        firstlast = firstlast.replace('# first and last eigenvalue to print','')
+        first, last = firstlast.split()
 
-    minmax = ei.readline()
-    minmax = minmax.replace('# xMin xMax nPoint','')
-    minmax = minmax.split()
-    xmin, xmax, xnum = minmax
-    xmin = float(xmin)
-    firstlast = ei.readline()
+        interpolation = data.readline()
+        interpolation = interpolation.replace('# interpolation type','')
+        numberinterpol = data.readline()
+        numberinterpol = numberinterpol.replace('# nr. of interpolation points and xy declarations','') #pylint: disable=line-too-long
+        rest = np.loadtxt(data.readlines())
 
-    interpolation = ei.readline()
+    return xmin, xmax, xnum, masse, rest, interpolation, numberinterpol, first, last
 
-    numberinterpol = ei.readline()
-
-    rest = ei.readlines()
-    
-    
-    ei.close()
-    print(xmin)
-    return  
-
-getdata("sciproject")
+print(getdata("sciproject"))
