fe624777ffdfe9dbd10518701fe8ea6038b663ef
JoniKanoni <jonasbottjer@mailbox.org>
2021-07-26 12:00:24 +0200

    optimization of test_reader
    code optimization

--- test_reader.py
+++ test_reader.py
@@ -1,5 +1,8 @@
-import reader
+'''
+Tests the reader.py
+'''
 import numpy as np
+import reader
 
 def test_unendlich():
     ''' Test 1 - Unendlich tiefer'''
@@ -11,12 +14,9 @@ def test_unendlich():
     lasttest = 5
     interpolationtest = 'linear'
     numinterpolationtest = 2
-    resttest = np.array([[-2.0, 0.0], [2.0, 0.0]]) 
-
-    masse, xmin, xmax, xnum, first, last, interpolation, numinterpol, rest = reader.getdata('sciproject')   
-
+    resttest = np.array([[-2.0, 0.0], [2.0, 0.0]])
+    masse, xmin, xmax, xnum, first, last, interpolation, numinterpol, rest = reader.getdata('sciproject')
     assert masse == massetest
-   
     assert xmin == xmintest
     assert xmax == xmaxtest
     assert xnum == npointtest
@@ -25,6 +25,3 @@ def test_unendlich():
     assert interpolation == interpolationtest
     assert numinterpol == numinterpolationtest
     assert np.array_equal(rest,resttest)
-
-    return
-
