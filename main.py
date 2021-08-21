af533f023f3a1c8eb23e8f074b124b9b7ab2c3d1
Nils Derner <nderner@uni-bremen.de>
2021-08-21 18:15:03 +0200

    Now Reader works with parser More Cleanup Plot now saves at given path Program is now executable from shell

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-"""
-Main routine for solving a stationary 1D Schroedinger equation
-"""
+#!/usr/bin/env python3
+""" Main routine for solving a stationary 1D Schroedinger equation """
 
 from modules import reader
 from modules import interpolate as ip
 from modules import solver
+import argparse
+
+_DESCRIPTION = "Solves a 1D Schrodinger equation for given potential."
 
 def main():
     '''
     Main function for solving a stationary 1D Schroedinger equation
     '''
+    
     #get data
-    mass, xmin, xmax, xnum, first_val, last_val, inttype, pot, inputpath = reader.get_data()
+    parser = argparse.ArgumentParser(description=_DESCRIPTION)
+    msg = 'Path to input file (default: .)'
+    parser.add_argument('-i', '--input', type=str, default='.', help=msg)
+    args = parser.parse_args()
+
+    mass, xmin, xmax, xnum, first_val, last_val, inttype, pot, inputpath = reader.get_data(args.input)
 
     #interpolate data and create hamiltonian
     potential = ip.potential_grid(xmin, xmax, xnum, pot[:, 0], pot[:, 1], inttype)
--- main_plot.py
+++ main_plot.py
@@ -1,15 +1,16 @@
-'''
-Main routine for plotting the results of the solver
-'''
+#!/usr/bin/env python3
+"""Main routine for plotting the results of the solver"""
 
 from modules import plotter
+
+_DESCRIPTION = "Plots given data from a 1-dimension qm-system."
 
 def main():
     '''
     Main routine for plotting the results of the solver
     '''
-    energies, wavefuncs, expvalues, potential = plotter.read_data()
-    plotter.ploty(energies, wavefuncs, expvalues, potential)
+    energies, wavefuncs, expvalues, potential, path = plotter.read_data()
+    plotter.ploty(energies, wavefuncs, expvalues, potential, path)
 
 if __name__ == '__main__':
     main()
